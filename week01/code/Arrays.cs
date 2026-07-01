3dda4aa1c51a8b1d8e100e9cc5fcd8d2b21bf339
jdethington <jdethington@gmail.com>
2026-07-01 14:00:12 -0700

    Week 01 Dynamic arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,20 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // 
+        // ==============================================================
+        // First, create a new array of doubles with the name "multiples" with the specified length of "length".
+        // Next, use a for loop that iterates through by the number of times specified by "length".  
+        // Inside the loop take the current index plus 1 and multiply that by the "number" and store that value in the array at the current index.
+        // Finally, return the array.
+        // ==============================================================
 
-        return []; // replace this return statement with your own
+        var multiples = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +41,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // ==============================================================
+        // Create a new list called "startData" that extracts the first part of the list "data" using the GetRange method, starting at index "0" and ending at index "data.Count - amount".
+        // Create another new list called "end Data" that extracts the last part of the list "data".  Again using the GetRange method, stating at index "data.Count - amount" and ending at index "amount".
+        // Clear the original list "data" using the Clear method.
+        // Add the "endData" list to the original list "data" using the AddRange method.
+        // Add the "startData" list to the original list "data" using the AddRange method.
+        // ==============================================================
+
+        var startData = data.GetRange(0, data.Count - amount);
+        var endData = data.GetRange(data.Count - amount, amount);
+        data.Clear();
+        data.AddRange(endData);
+        data.AddRange(startData);
     }
 }
